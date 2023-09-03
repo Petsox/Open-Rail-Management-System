800caf43b595438edb5c36303e65cd4fa967b730
tpeterka1 <tpeterka1@gmail.com>
2023-09-03 21:40:10 +0200

    Indentation fixes

--- ORMS.lua
+++ ORMS.lua
@@ -1,14 +1,14 @@
 local component = require("component")
 local gpu = component.gpu
-gpu.setResolution(160,50)
+gpu.setResolution(160, 50)
 local gui = require("gui")
 local term = require("term")
 local SaS = require("SaS")
 local computer = require("computer")
 local ormslib = require("ormsLib")
 
-gui.checkVersion(2,5)
- 
+gui.checkVersion(2, 5)
+
 local prgName = "RMS"
 local version = "v1.0"
 local white = 0xffffff
@@ -18,22 +18,22 @@ local red = 0xff0000
 -- Begin: Callbacks
 
 local function exitButtonCallback(guiID, id)
-   local result = gui.getYesNo("", "Do you really want to exit?", "")
-   if result == true then
-      SaS.reset()
-      term.clear()
-      os.exit()
-      --computer.shutdown()
-   end
-   gui.displayGui(mainGui)
+	local result = gui.getYesNo("", "Do you really want to exit?", "")
+	if result == true then
+		SaS.reset()
+		term.clear()
+		os.exit()
+		--computer.shutdown()
+	end
+	gui.displayGui(mainGui)
 end
 
 local function Switch(guiID, widgetID, name)
-   ormslib.Switch(mainGui, widgetID, gui.getName(mainGui, widgetID))
+	ormslib.Switch(mainGui, widgetID, gui.getName(mainGui, widgetID))
 end
 
 local function Signal(name, widgetID)
-   ormslib.Signal(gui.getName(mainGui, widgetID), widgetID)
+	ormslib.Signal(gui.getName(mainGui, widgetID), widgetID)
 end
 
 -- End: Callbacks
@@ -52,5 +52,5 @@ gui.setBottom("Made with Gui library v2.5 reworked and rewritten by Petsox")
 
 -- Main loop
 while true do
-   gui.runGui(mainGui)
+	gui.runGui(mainGui)
 end
