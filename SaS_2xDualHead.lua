6299ad3d0841e281d1a0a09425b3adb969f68355
tpeterka1 <tpeterka1@gmail.com>
2023-09-06 22:14:16 +0200

    Added SaS config for custom 2x dual head signals (Czech) - needs a redstone contraption

---
+++ SaS_2xDualHead.lua
@@ -0,0 +1,43 @@
+local gui = require("gui")
+local component = require("component")
+
+local boxVy = component.proxy(component.get("511d")) -- Switch controller
+local boxNa = component.proxy(component.get("9a29")) -- Signal controller (green/red)
+local boxNaVy = component.proxy(component.get("8682")) -- Signal controller (yellow)
+
+local SaS = {}
+
+function SaS.reset()
+  boxVy.setEveryAspect(5)
+  boxNa.setEveryAspect(5)
+  boxNaVy.setEveryAspect(1)
+end
+
+SaS.reset()
+
+--Signals
+
+function SaS.Red(signalName)
+  boxNa.setAspect(signalName, 5)
+  boxNaVy.setAspect(signalName, 5)
+end
+
+function SaS.Yellow(signalName)
+  boxNa.setAspect(signalName, 1)
+  boxNaVy.setAspect(signalName, 3)
+end
+
+function SaS.Green(signalName)
+  boxNa.setAspect(signalName, 1)
+  boxNaVy.setAspect(signalName, 5)
+end
+
+function SaS.switchFrom(switchName)
+  boxVy.setAspect(switchName, 5)
+end
+
+function SaS.switchTo(switchName)
+  boxVy.setAspect(switchName, 1)
+end
+
+return SaS
